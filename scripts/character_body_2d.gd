d86cc1ed6f7aba8f2d26b205b82f52a7ff4486e4
proxin187 <thugshaker627@gmail.com>
2026-01-12 13:29:22 +0100

    feat: walk animation

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
-var speed = 200
+var speed = 250
 var jump_velocity = -400
 var gravity = 980
 
 func _ready():
-	$AnimatedSprite2D.play("default")
+	$AnimatedSprite2D.play("idle")
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	#if not is_on_floor():
+	#	velocity.y += gravity * delta
 
 	if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
 		velocity.y = jump_velocity
@@ -24,5 +24,15 @@ func _physics_process(delta):
 		velocity.x = direction * speed
 	else:
 		velocity.x = 0
-	
+
 	move_and_slide()
+
+	if direction != 0:
+		$AnimatedSprite2D.play("walk")
+	else:
+		$AnimatedSprite2D.play("idle")
+
+	if direction < 0:
+		$AnimatedSprite2D.flip_h = false
+	elif direction > 0:
+		$AnimatedSprite2D.flip_h = true
