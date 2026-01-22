60e940b79e7c684bf18c02763406afe9621bec65
proxin187 <thugshaker627@gmail.com>
2026-01-22 13:26:54 +0100

    feat: inventory

---
+++ scripts/area_2d.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+
+	var tween = create_tween()
+	tween.set_loops()
+	tween.tween_property(self, "position:y", position.y - 50, 1.0).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "position:y", position.y, 1.0).set_trans(Tween.TRANS_SINE)
+
+func _on_body_entered(body: Node2D):
+	$"../CanvasLayer/Inventory".add_item("potion")
+	queue_free()
