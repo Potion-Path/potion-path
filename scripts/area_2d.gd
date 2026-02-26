6336039446b0c5b38abe35657ef0db100c2acbae
proxin187 <thugshaker627@gmail.com>
2026-02-26 12:32:58 +0100

    feat: arena, and PASSIVE AGRESSIVE LICENSE

--- scripts/area_2d.gd
+++ scripts/area_2d.gd
@@ -10,5 +10,5 @@ func _ready():
 	tween.tween_property(self, "position:y", position.y, 1.0).set_trans(Tween.TRANS_SINE)
 
 func _on_body_entered(_body: Node2D):
-	#$"../CanvasLayer/Inventory".add_item("potion")
+	$"../CanvasLayer/Inventory".add_item("potion")
 	queue_free()
